e118c987f067227e13095656230490ad586306b0
Mac os <macos@MacdeMacBook-Pro-2.local>
2018-05-24 12:40:37 +0800

    finish the security

--- src/main/java/com/agriculture/youcai/config/WebSecurityConfig.java
+++ src/main/java/com/agriculture/youcai/config/WebSecurityConfig.java
@@ -1,25 +1,18 @@
 package com.agriculture.youcai.config;
 
 import com.agriculture.youcai.enums.ResultEnum;
-import com.agriculture.youcai.service.GuestService;
 import com.agriculture.youcai.service.impl.GuestServiceImpl;
 import com.agriculture.youcai.utils.ResultVOUtils;
 import com.agriculture.youcai.vo.ResultVO;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -29,7 +22,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
